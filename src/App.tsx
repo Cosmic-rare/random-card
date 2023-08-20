a78a51f1377e732abc27011336ba1e45f2c06e4e
Cosmic-rare <ysolakey1116@gmail.com>
2023-08-20 17:15:38 +0900

    create application

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { FullScreen, useFullScreenHandle } from 'react-full-screen'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const defaultInterval = 5000
+  const localStorageKey = 'content'
+  const message = `Enter the text, separatig each item with line break.\nlike\nthis.`
+  const previousData = localStorage.getItem(localStorageKey)
+  const handle = useFullScreenHandle()
+  const [content, setContent] = useState<string>(previousData !== null ? previousData : message)
+  const [items, setItems] = useState<string[]>([])
+  const [item, setItem] = useState<string>('')
+  const [inter, setInter] = useState<number>(defaultInterval)
+  const [time, setTime] = useState<string>((defaultInterval / 1000).toString())
+
+  const handleOnTimeUpdate = () => {
+    const parsedTime = parseFloat(time)
+    if (!isNaN(parsedTime) && parsedTime > 0) {
+      setInter(parsedTime * 1000)
+    } else {
+      setInter(defaultInterval)
+      setTime((defaultInterval / 1000).toString())
+    }
+  }
+
+  useEffect(() => {
+    const preItems = content.split(/\r\n|\n/)
+    setItems(preItems.filter((item) => item !== ''))
+    localStorage.setItem(localStorageKey, content)
+  }, [content])
+
+  const setRandomItem = () => {
+    setItem(items[Math.floor(Math.random() * items.length)])
+  }
+
+  useEffect(() => {
+    const timerId = setInterval(setRandomItem, inter)
+    return () => clearInterval(timerId)
+  }, [items, inter])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2>Content</h2>
+        <textarea rows={15} cols={60} value={content} onChange={(e) => setContent(e.target.value)}></textarea>
+      </div>
+      <div>
+        <h2>Interval</h2>
+        <p>Update every {inter / 1000} seconds.</p>
+        <input
+          type='number'
+          value={time}
+          onChange={(e) => setTime(e.target.value)}
+        />
+        <button onClick={handleOnTimeUpdate}>Apply</button>
+      </div>
+      <div>
+        <h2>FullScreen</h2>
+        <button onClick={handle.enter}>Open</button>
+      </div>
+      <FullScreen handle={handle}>
+        {handle.active && (
+          <>
+            <h1>{item ? item : '...'}</h1>
+            <button onClick={handle.exit}>Close</button>
+          </>
+        )}
+      </FullScreen>
     </div>
   );
 }
